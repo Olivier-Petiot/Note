546856ef056bbaa0bdb7661c6a8bad83dd283a99
opetiot <opetiot@gmail.com>
2015-05-25 17:05:05 +0000

    Add User_Id to Post records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525144516) do
+ActiveRecord::Schema.define(version: 20150525163912) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150525144516) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
